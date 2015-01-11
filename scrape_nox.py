6ea0eeaf83c1fe5b0005b15a4035e1d3bc37bd4d
Thomas Taranowski <tom@baringforge.com>
2015-01-11 01:04:22 -0800

    Forgot about federal noxious weed column :/

--- scrape_nox.py
+++ scrape_nox.py
@@ -12,16 +12,17 @@ rows = soup.select('tr.rowon')
 
 csvfile = open('noxious_plants.csv', 'wb') 
 csv_writer = csv.writer(csvfile, delimiter=',')
-csv_writer.writerow(['Scientific Name', 'Common Name', 'Noxious Status', 'Native Status'])
+csv_writer.writerow(['Scientific Name', 'Common Name', 'Federal Noxious Status', 'State Noxious Status', 'Native Status'])
 
 for r in rows:
     cols = r.findAll('td')
     sciname = cols[0].text
     comname = cols[1].text
-    nox     = cols[2].text
-    native  = cols[3].text
+    fnox    = cols[2].text
+    snox    = cols[3].text
+    native  = cols[4].text
 
-    csv_writer.writerow([sciname.encode('utf8'), comname, nox, native])
+    csv_writer.writerow([sciname.encode('utf8'), comname, fnox, snox, native])
 
 
 
